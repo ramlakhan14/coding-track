4d7c1ca7484d506a971e5a6507d785028198d679
Ramlakhan Kumar <70676290+ramlakhan14@users.noreply.github.com>
2026-06-20 21:14:46 +0530

    Time: 5 ms (55.49%) | Memory: 60.6 MB (81.41%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+   const roman={
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000
+};
+let result=0;
+for(let i=0;i<s.length;i++){
+    let currNum=roman[s[i]];
+    let nextNum=roman[s[i+1]];
+
+    if(currNum<nextNum){
+        result -= currNum;
+
+    }else{
+result +=currNum;
+    }
+}
+return result;
+};
